340b09ea4476acfb849110f0da41f390e071d37a
IonitaCatalin <ionita.catalin2000@gmail.com>
2022-12-11 12:51:27 +0200

    fix(build): Fix typo

--- scripts/semantic-release/docker/build.sh
+++ scripts/semantic-release/docker/build.sh
@@ -13,21 +13,21 @@ export VERSION=$(node -p -e "require('./package.json').version")
 docker login -u khonsu03 -p $DOCKERHUB_TOKEN
 
 # Install & build all workspaces
-sh ./scripts/docker/build.sh "now-services-core-builder" "scripts/semantic-release/docker/Dockerfile" &
+./scripts/docker/build.sh "now-services-core-builder" "scripts/semantic-release/docker/Dockerfile" &
 
 # Build & push now-services-daemon-sync image
-sh ./scripts/docker/build.sh "now-services-daemon-sync" "apps/daemons/sync/Dockerfile" &
+./scripts/docker/build.sh "now-services-daemon-sync" "apps/daemons/sync/Dockerfile" &
 
 # Build & push now-services-daemon-geolocation image
-sh ./scripts/docker/build.sh "now-services-daemon-geolocation" "apps/daemons/geolocation/Dockerfile" &
+./scripts/docker/build.sh "now-services-daemon-geolocation" "apps/daemons/geolocation/Dockerfile" &
 
 # Build & push now-services-api-sparql image
-sh ./scripts/docker/build.sh "now-services-api-sparql" "apps/api/sparql/Dockerfile" &
+./scripts/docker/build.sh "now-services-api-sparql" "apps/api/sparql/Dockerfile" &
 
 # Build & push now-services-api-mgt image
-sh ./scripts/docker/build.sh"now-services-api-mgt" "apps/api/mgt/Dockerfile" &
+./scripts/docker/build.sh "now-services-api-mgt" "apps/api/mgt/Dockerfile" &
 
 # Build & push now-services-cli image
-sh ./scripts/docker/build.sh "now-services-cli" "apps/cli/Dockerfile" &
+./scripts/docker/build.sh "now-services-cli" "apps/cli/Dockerfile" &
 
 
